8ea2f12ae69c0308d7bea24228436d210d2ebcce
hh120901 <70877597+hh120901@users.noreply.github.com>
2022-08-19 17:28:01 +0700

    AddIcons

--- cloneins/src/Heading.jsx
+++ cloneins/src/Heading.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import './Heading.css'
+import { House, Mess  } from "./Icons";
+
+
 function Heading() {
   return (
     <>
       <div className="heading__homeIcon">
         <h2>𝒟𝓇𝑒𝒶𝓂𝑔𝓇𝒶𝓂</h2>
       </div>
-      <div className="headingn__aIcon">some icons</div>
+      <div className="headingn__aIcon">
+        
+        <a href="https://www.instagram.com/">
+          <House />
+        </a>
+        <a href="https://www.instagram.com/direct/inbox/">
+          <Mess />
+        </a>
+      </div>
     </>
   );
 }
