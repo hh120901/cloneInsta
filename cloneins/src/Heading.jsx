8a3e05a345a8d56725e65283db648a56ebee87fe
hh120901 <70877597+hh120901@users.noreply.github.com>
2022-08-19 17:34:29 +0700

    AddIcons++

--- cloneins/src/Heading.jsx
+++ cloneins/src/Heading.jsx
@@ -17,6 +17,10 @@ function Heading() {
         <a href="https://www.instagram.com/direct/inbox/">
           <Mess />
         </a>
+        <img
+            src="https://via.placeholder.com/30"
+            alt="avatar"
+          />
       </div>
     </>
   );
